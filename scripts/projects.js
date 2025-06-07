6f28dfde92901ce9ddd7a9969cf1ddcf90412301
Ignacio Dias Gundin <ignadiasg@gmail.com>
2025-06-07 15:06:42 -0300

    bug with image fixed

--- scripts/projects.js
+++ scripts/projects.js
@@ -6,7 +6,7 @@ const PROJECTS = [
         description: 'Monkeytype es una plataforma en línea para practicar mecanografía. Ofrece ejercicios de tipeo en diferentes modos (palabras, oraciones, código, etc.) y permite medir la velocidad (WPM), precisión y consistencia del usuario. Es conocida por su diseño minimalista, alta personalización y estadísticas detalladas. Es popular entre quienes quieren mejorar su velocidad de escritura y competir con otros.',
         link: 'https://ignaciodias.github.io/monkeytype-clone/',
         github: 'https://github.com/ignacioDias/monkeytype-clone/',
-        image: '../src/monkeytype-clone.webp',
+        image: 'src/monkeytype-clone.webp',
         tags: ['TECH', 'CSS', 'JS']
     }
 ]
