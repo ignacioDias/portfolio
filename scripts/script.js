d8d83936d41233c96c38926e24b97618f4331794
Ignacio Dias Gundin <ignadiasg@gmail.com>
2025-06-08 14:24:24 -0300

    bugs con correo y contacto

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const mailBtn = document.getElementById('mail-btn');
+  const mailBtns = document.getElementsByClassName('mail-btn');
   const toast = document.getElementById('toast-mail');
   const email = 'ignaciodiasgundin@gmail.com';
 
-  mailBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText(email);
+  Array.from(mailBtns).forEach(mailBtn => {
+    mailBtn.addEventListener('click', () => {
+      navigator.clipboard.writeText(email);
 
-    toast.style.display = 'flex';
-    toast.textContent = '¡Correo copiado!';
-    setTimeout(() => {
-      toast.style.display = 'none';
-    }, 1800);
+      toast.style.display = 'flex';
+      toast.textContent = '¡Correo copiado!';
+      setTimeout(() => {
+        toast.style.display = 'none';
+      }, 1800);
+    });
   });
 });
